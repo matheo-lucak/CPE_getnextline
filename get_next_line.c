d8d591e6bd4bd37cee3e244c0adab93350701ceb
LUCAK Matheo <matska@localhost.localdomain>
2019-12-18 20:44:18 +0100

    [FIX] Now get last line

--- get_next_line.c
+++ get_next_line.c
@@ -19,57 +19,56 @@ int is_end_of_line(char *buffer)
     return (0);
 }
 
-char *my_strdup_line(char *line, char **buffer)
+char *my_strdup_line(char *line, char *buffer)
 {
     int i = 0;
     int j = 0;
 
-    while ((*buffer)[i] != '\0' && (*buffer)[i] != '\n')
+    while (buffer[i] != '\0' && buffer[i] != '\n')
         i++;
     line = malloc(sizeof(char) * (i + 1));
     if (line == NULL)
         return (NULL);
     while (j < i) {
-        line[j] = (*buffer)[j];
+        line[j] = buffer[j];
         j++;
     }
     line[j] = '\0';
-    (*buffer) += j + 1;
     return (line);
 }
 
-char *re_alloc_buffer(char *buffer, char *stock)
+char *re_alloc_buffer(char *buffer, char *stock, int mode)
 {
     char *new_buffer = NULL;
     int buffer_size = -1;
     int j = -1;
 
-    while (buffer[++buffer_size] != '\0');
+    while (buffer[++buffer_size] != '\0' && mode == 1);
     new_buffer = malloc(sizeof(char) * (buffer_size + READ_SIZE + 1));
     if (new_buffer == NULL)
         return (NULL);
-    while (++j < buffer_size)
+    while (++j < buffer_size && mode == 1)
         new_buffer[j] = buffer[j];
     j = -1;
     while (stock[++j] != '\0')
         new_buffer[j + buffer_size] = stock[j];
     new_buffer[j + buffer_size] = '\0';
+    free(buffer);
     return (new_buffer);
 }
 
-char *add_in_buffer(int fd, char *buffer)
+char *add_in_buffer(int fd, char *buffer, int *rd)
 {
     char stock[READ_SIZE + 1];
-    int rd = -1;
 
     if (stock == NULL)
         return (NULL);
-    while (!is_end_of_line(buffer)) {
-        rd = read(fd, stock, READ_SIZE);
-        if (rd == -1)
+    while (!is_end_of_line(buffer) && *rd != 0) {
+        *rd = read(fd, stock, READ_SIZE);
+        if (*rd == -1)
             return (NULL);
-        stock[READ_SIZE] = '\0';
-        buffer = re_alloc_buffer(buffer, stock);
+        stock[*rd] = '\0';
+        buffer = re_alloc_buffer(buffer, stock, 1);
     }
     return (buffer);
 }
@@ -78,8 +77,8 @@ char *get_next_line(int fd)
 {
     static char *buffer = NULL;
     char * line = NULL;
-    int i = 0;
-    int j = -1;
+    int i = -1;
+    int rd = -1;
 
     if (fd == -1)
         return (NULL);
@@ -87,9 +86,13 @@ char *get_next_line(int fd)
         buffer = malloc(sizeof(char) * (READ_SIZE + 1));
         buffer[0] = '\0';
     }
-    buffer = add_in_buffer(fd, buffer);
-    if (buffer == NULL)
+    buffer = add_in_buffer(fd, buffer, &rd);
+    if (buffer == NULL || (buffer[0] == '\0' && rd == 0))
         return (NULL);
-    line = my_strdup_line(line, &buffer);
+    line = my_strdup_line(line, buffer);
+    while (line[++i] != '\0' && line[i] != '\n');
+    buffer = re_alloc_buffer(buffer, buffer + i + 1, 0);
+    if (line == NULL)
+        return (NULL);
     return (line);
 }
