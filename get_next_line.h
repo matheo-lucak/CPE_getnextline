01220b46d0e2e90026cf55772de435e8c6761c48
matheo.lucak <matheo.lucak@epitech.eu>
2019-12-20 18:20:16 +0100

    [FIX] Redifine READ_SIZE

--- get_next_line.h
+++ get_next_line.h
@@ -5,13 +5,15 @@
 ** header for duck hunt
 */
 
-#include <unistd.h>
-#include <stdlib.h>
-
 #ifndef GET_NEXT_LINE_H_
 #define GET_NEXT_LINE_H_
 
-#define READ_SIZE (3)
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifndef READ_SIZE
+#define READ_SIZE (1234)
+#endif
 
 char *get_next_line(int fd);
 
